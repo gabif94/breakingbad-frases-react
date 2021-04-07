b89e13e72e87e425260edd4942997b4e3e0b5e82
gabif94 <gabi94f@gmail.com>
2021-04-07 14:06:14 -0300

    proyecto finalizado, consumo de API y useEffect utilizado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import {useState, useEffect} from 'react';
 import styled from '@emotion/styled';
+import Frase from './components/Frase';
 
 const Contenedor = styled.div`
 	display: flex;
@@ -21,17 +23,31 @@ const Boton = styled.button`
 	padding: 1rem 3rem;
 	font-size: 2rem;
 	border: 2px solid black;
+	transition: background-size 0.8s ease;
+
+	:hover {
+		cursor: pointer;
+		background-size: 400;
+	}
 `;
 function App() {
+	//state de frases
+	const [frase, setFrase] = useState({});
 	const consultarApi = async () => {
 		const api = await fetch(
 			'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
 		);
 		const frase = await api.json();
-		console.log(frase[0]);
+		setFrase(frase[0]);
 	};
+
+	//Cargar una frase
+	useEffect(() => {
+		consultarApi();
+	}, []);
 	return (
 		<Contenedor>
+			<Frase frase={frase} />
 			<Boton onClick={consultarApi}>Obtener Frase</Boton>
 		</Contenedor>
 	);
